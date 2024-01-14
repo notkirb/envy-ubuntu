74321844f2ac3873b93747110bf272d52450866c
notkirb_ <90074338+notkirb@users.noreply.github.com>
2024-01-13 22:42:45 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-#Install google chrome, xorg, xserver-xorg-legacy, openbox and pulseaudio
+#Install google chrome, xorg, xserver-xorg-legacy, zip, openbox and pulseaudio
 sudo add-apt-repository 'deb http://dl.google.com/linux/chrome/deb/ stable main'
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
 sudo apt update -y
-sudo apt install -y --no-install-recommends xorg openbox google-chrome-stable pulseaudio xserver-xorg-legacy
+sudo apt install -y --no-install-recommends xorg openbox google-chrome-stable pulseaudio xserver-xorg-legacy zip
 sudo usermod -a -G audio $USER
 
 #Install xserver-xorg-legacy and choose "Anybody"
@@ -17,9 +17,9 @@ sudo systemctl enable getty@tty1 getty@tty2 getty@tty3
 sudo cp grub /etc/default/grub
 sudo update-grub
 
-# Update Grub
-sudo cp grub /etc/default/grub
-sudo update-grub
+#Download Envy
+curl https://cdn.discordapp.com/attachments/1175399709880361132/1185309816361599039/beta2.zip?ex=65b40eca&is=65a199ca&hm=bf5b08fd37521b5662203ad20480cdb36b1595cb67288ffd8fef6920691a4b7d&
+unzip beta2.zip/envy ~/
 
 #install kiosh service
 sudo cp kiosk.sh /opt/
@@ -27,3 +27,7 @@ sudo chmod +x /opt/kiosk.sh
 sudo cp kiosk.service /etc/systemd/system/
 sudo systemctl enable kiosk.service
 sudo systemctl start kiosk.service
+
+#clean up
+cd ~
+rm -rf ~/envy-ubuntu
