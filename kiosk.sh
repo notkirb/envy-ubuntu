b9864d8669578906cc134cc6e5ce147c446477ef
notkirb_ <90074338+notkirb@users.noreply.github.com>
2024-01-14 09:50:22 -0700

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -7,5 +7,5 @@ start-pulseaudio-x11
 
 while true; do
   rm -rf ~/.{config,cache}/google-chrome/
-  google-chrome --kiosk --no-first-run  '~/envy/index.html'
+  google-chrome --kiosk --no-first-run  'https://envy.js.org/'
 done
