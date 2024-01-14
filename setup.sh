c02c5c7823c723664a99b9778ade59d13dc19476
notkirb_ <90074338+notkirb@users.noreply.github.com>
2024-01-13 23:26:51 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,9 @@ sudo cp grub /etc/default/grub
 sudo update-grub
 
 #Download Envy
-curl -O https://cdn.discordapp.com/attachments/1175399709880361132/1185309816361599039/beta2.zip?ex=65b40eca&is=65a199ca&hm=bf5b08fd37521b5662203ad20480cdb36b1595cb67288ffd8fef6920691a4b7d& && unzip beta2.zip* -d ..
+curl -O -L https://cdn.discordapp.com/attachments/1175399709880361132/1185309816361599039/beta2.zip?ex=65b40eca&is=65a199ca&hm=bf5b08fd37521b5662203ad20480cdb36b1595cb67288ffd8fef6920691a4b7d& 
+sleep 3
+unzip beta2.zip* -d ..
 
 #install kiosh service
 sudo cp kiosk.sh /opt/
