8929f61f32ea97195ae8c4bc27db03fb1fca9ab1
notkirb_ <90074338+notkirb@users.noreply.github.com>
2024-01-13 23:17:34 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ sudo cp grub /etc/default/grub
 sudo update-grub
 
 #Download Envy
-curl -o ~/envy-ubuntu/envy.zip https://cdn.discordapp.com/attachments/1175399709880361132/1185309816361599039/beta2.zip?ex=65b40eca&is=65a199ca&hm=bf5b08fd37521b5662203ad20480cdb36b1595cb67288ffd8fef6920691a4b7d&
+curl -o ./envy-ubuntu/envy.zip https://cdn.discordapp.com/attachments/1175399709880361132/1185309816361599039/beta2.zip?ex=65b40eca&is=65a199ca&hm=bf5b08fd37521b5662203ad20480cdb36b1595cb67288ffd8fef6920691a4b7d&
 unzip envy.zip -d ~/
 
 #install kiosh service
@@ -29,5 +29,5 @@ sudo systemctl enable kiosk.service
 sudo systemctl start kiosk.service
 
 #clean up
-cd ~
+cd ..
 sudo rm -rf ~/envy-ubuntu
